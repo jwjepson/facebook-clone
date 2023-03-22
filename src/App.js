0e3d84a4bdc2b1a8bf3c16ff2f146da9ed4f18d6
Jake Jepson <jwjepson1@gmail.com>
2023-03-22 17:37:12 -0400

    create a react fragment

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import Login from "./components/Login";
+import Signup from "./components/Signup";
 import "./styles/app.css";
 
 const App = () => {
   return (
-    <Login/>
+    <>
+      <Login/>
+    </>
   );
 }
 
