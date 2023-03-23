26568f0bf415d8a5af1ebfaa894a3645ec921ed5
Jake Jepson <jwjepson1@gmail.com>
2023-03-22 23:12:24 -0400

    import icons and put inside button tags for header buttons

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,21 @@
 import React from "react";
 import "../styles/header.css";
 import logo from "../icons/fb-clone-logo.png";
-import searchIcon from "../icons/search-icon.svg";
+import notificationIcon from "../icons/notification-icon.svg";
+import chatIcon from "../icons/chat-icon.svg";
+import defaultProfile from "../images/default-profile-pic.jpg";
 
 const Header = () => {
     return (
         <div className="header">
-            <img className="logo-small" src={logo} alt="logo"></img>
-            <div>
+            <div className="left-info">
+                <img className="logo-small" src={logo} alt="logo"></img>
                 <input className="search-box" placeholder="Search Facebook Clone" autoComplete="off" type="search" name="search"></input>
+            </div>
+            <div className="right-info">
+                <button className="header-button" type="button" name="chat-button"><object type="image/svg+xml" data={chatIcon}></object></button>
+                <button className="header-button" type="button" name="notifcation-button"><object type="image/svg+xml" data={notificationIcon}></object></button>
+                <button className="header-button" type="button" name="profile-button"><img src={defaultProfile}></img></button>
             </div>
         </div>
     )
