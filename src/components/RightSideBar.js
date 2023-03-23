db59fa989d942f60ee506598d49699168e54727c
Jake Jepson <jwjepson1@gmail.com>
2023-03-23 19:38:12 -0400

    create right sidebar component

---
+++ src/components/RightSideBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Birthdays from "./Birthdays";
+
+const RightSideBar = () => {
+    return (
+        <div className="right-sidebar">
+            <Birthdays/>
+        </div>
+    )
+}
+
+export default RightSideBar;
