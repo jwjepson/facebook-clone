f4dd6c24258b778f00aec5a474635984336b9254
Jake Jepson <jwjepson1@gmail.com>
2023-03-23 01:03:15 -0400

    create status creator component

---
+++ src/components/StatusCreator.js
@@ -0,0 +1,22 @@
+import React from "react";
+import profilePic from "../images/default-profile-pic.jpg";
+import "../styles/statuscreator.css";
+import addPhotoIcon from "../icons/add-photo-icon.svg";
+
+const StatusCreator = () => {
+    return (
+        <div className="status-creator-container">
+            <div className="top-info">
+                <img className="header-button" src={profilePic}></img>
+                <button className="status-creator-button" type="button" name="status-button">What's on your mind, User?</button>
+            </div>
+            <div className="bottom-options">
+                <ul>
+                    <li><img className="add-photo-icon" src={addPhotoIcon}></img>Photo/video</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default StatusCreator;
