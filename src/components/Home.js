6ba62f13b3037d741cd4e0f0973e6fad47c94915
Jake Jepson <jwjepson1@gmail.com>
2023-03-25 02:48:21 -0400

    toggle createPostDisplay on and off using state and conditional rendering

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,22 +1,34 @@
-import React from "react";
+import React, {useState} from "react";
 import SideNav from "./SideNav";
 import StatusCreator from "./StatusCreator";
 import Status from "./Status";
 import LeftSideBar from "./LeftSideBar";
 import RightSideBar from "./RightSideBar";
+import CreatePost from "./CreatePost";
 import "../styles/home.css";
 
 const Home = () => {
+    const [createPostDisplay, setcreatePostDisplay] = useState(false);
+
+    const toggleCreatePost = () => {
+        setcreatePostDisplay(!createPostDisplay);
+    }
+
     return (
-        <div className="home-page">
+        <>
+        {createPostDisplay && (
+            <CreatePost close={toggleCreatePost}/>
+        )}
+        <div className={`home-page ${createPostDisplay ? "overlay" : ""}`}>
             <LeftSideBar/>
             <div className="timeline">
-                <StatusCreator/>
+                <StatusCreator open={toggleCreatePost}/>
                 <Status/>
                 <Status/>
             </div>
             <RightSideBar/>
         </div>
+        </>
     )
 }
 
