886ebf38d216ed7ec78f75c67addcf88c712ce5d
Jake Jepson <jwjepson1@gmail.com>
2023-03-25 17:21:41 -0400

    create notifications component

---
+++ src/components/Notifications.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "../styles/notifications.css";
+import moreIcon from "../icons/more-icon.svg";
+import profilePic from "../images/default-profile-pic.jpg";
+
+const Notifications = () => {
+    return (
+        <div className="notifications-container">
+            <div className="notifications-header">
+                <h4 className="notifications-title">Notifications</h4>
+                <img src={moreIcon} className="more-button notifcations"></img>
+            </div>
+            <div className="notifications">
+                <div className="see-all-notifications">
+                    <h4>Most recent</h4>
+                    <a className="see-all-notifications-link">See all</a>
+                </div>
+                <ul className="notifications-list">
+                    <li className="notification">
+                        <img className="notification-avatar" src={profilePic}></img>
+                        <div className="notification-content">This is a test notifcation</div>
+                    </li>
+                    <li className="notification">
+                        <img className="notification-avatar" src={profilePic}></img>
+                        <div className="notification-content">This is a test notifcation</div>
+                    </li>
+                    <li className="notification">
+                        <img className="notification-avatar" src={profilePic}></img>
+                        <div className="notification-content">This is a test notifcation</div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Notifications;
