4c43d02584bdb6b2cb46883c09f317d2e85256d8
Jake Jepson <jwjepson1@gmail.com>
2023-03-26 19:38:51 -0400

    add link to user profile route

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -2,14 +2,17 @@ import React from "react";
 import defaultProfile from "../images/default-profile-pic.jpg";
 import friendsIcon from "../icons/friends-icon.svg";
 import groupsIcon from "../icons/groups-icon.svg";
+import { Link } from "react-router-dom";
 
 const SideNav = () => {
     return (
         <div className="sidenav-container">
             <ul>
-                <li><img className="sidenav-icons"src={defaultProfile}></img>Username</li>
-                <li><img className="sidenav-icons"src={friendsIcon}></img>Friends</li>
-                <li><img className="sidenav-icons"src={groupsIcon}></img>Groups</li>
+                <Link to="/username">
+                    <li className="sidenav-item"><img className="sidenav-icons"src={defaultProfile}></img>Username</li>
+                </Link>
+                <li className="sidenav-item"><img className="sidenav-icons"src={friendsIcon}></img>Friends</li>
+                <li className="sidenav-item"><img className="sidenav-icons"src={groupsIcon}></img>Groups</li>
             </ul>
         </div>
     )
