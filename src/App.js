e086eb35b4ebfac93434ee9813aebdac8cd21af9
Jake Jepson <jwjepson1@gmail.com>
2023-03-26 19:40:09 -0400

    set up browser routes and conditionally render login screen/home whether user is logged in

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import Login from "./components/Login";
 import Signup from "./components/Signup";
+import Header from "./components/Header";
+import Status from "./components/Status";
+import SideNav from "./components/SideNav";
+import WriteComment from "./components/WriteComment";
+import StatusCreator from "./components/StatusCreator";
+import Home from "./components/Home";
+import CreatePost from "./components/CreatePost";
+import Notifications from "./components/Notifications";
+import ProfileHeader from "./components/ProfileHeader";
+import Profile from "./components/Profile";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./styles/app.css";
 
 const App = () => {
+
+  const isLoggedIn = true;
   return (
     <>
-      <Login/>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={isLoggedIn ? <Home/> : <Login/>}/>
+        <Route path="/username" element={<Profile/>}/>
+      </Routes>
+    </BrowserRouter>
     </>
   );
 }
