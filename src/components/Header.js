a90386960dbb56ea2719714c11911ed1ccbddc3b
Jake Jepson <jwjepson1@gmail.com>
2023-03-26 19:37:37 -0400

    add links to home & user profile routes

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,7 @@ import notificationIcon from "../icons/notification-icon.svg";
 import chatIcon from "../icons/chat-icon.svg";
 import Notifications from "./Notifications";
 import defaultProfile from "../images/default-profile-pic.jpg";
+import { Link } from "react-router-dom";
 
 const Header = () => {
     const [notificationsDisplay, setnotificationsDisplay] = useState(false);
@@ -20,13 +21,17 @@ const Header = () => {
         )}
         <div className="header">
             <div className="left-info">
-                <img className="logo-small" src={logo} alt="logo"></img>
+                <Link to="/">
+                    <img className="logo-small" src={logo} alt="logo"></img>
+                </Link>
                 <input className="search-box" placeholder="Search Facebook Clone" autoComplete="off" type="search" name="search"></input>
             </div>
             <div className="right-info">
                 <button className="header-button" type="button" name="chat-button"><img src={chatIcon}></img></button>
                 <button onClick={toggleNotificationsDisplay} className="header-button" type="button" name="notification-button"><img src={notificationIcon}></img></button>
-                <button className="header-button" type="button" name="profile-button"><img src={defaultProfile}></img></button>
+                <Link to="/username">
+                    <button className="header-button" type="button" name="profile-button"><img src={defaultProfile}></img></button>
+                </Link>
             </div>
         </div>
         </>
