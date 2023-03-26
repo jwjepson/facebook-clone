d1afb8d75e4af07a4746b6a642bdabe00e1c1a66
Jake Jepson <jwjepson1@gmail.com>
2023-03-26 19:36:56 -0400

    import header and profileheader components

---
+++ src/components/Profile.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ProfileHeader from "./ProfileHeader";
+import Header from "./Header";
+
+const Profile = () => {
+    return (
+        <>
+        <Header/>
+        <ProfileHeader/>
+        </>
+    )
+}
+
+export default Profile;
