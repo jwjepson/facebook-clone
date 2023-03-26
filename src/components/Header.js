b870d86b6c70e9861e9cf8d009ba1d7a2871c2bc
Jake Jepson <jwjepson1@gmail.com>
2023-03-25 21:47:57 -0400

    conditionally render notifications modal

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,35 @@
-import React from "react";
+import React, {useState} from "react";
 import "../styles/header.css";
 import logo from "../icons/fb-clone-logo.png";
 import notificationIcon from "../icons/notification-icon.svg";
 import chatIcon from "../icons/chat-icon.svg";
+import Notifications from "./Notifications";
 import defaultProfile from "../images/default-profile-pic.jpg";
 
 const Header = () => {
+    const [notificationsDisplay, setnotificationsDisplay] = useState(false);
+
+    const toggleNotificationsDisplay = () => {
+        setnotificationsDisplay(!notificationsDisplay);
+    }
+
     return (
+        <>
+        {notificationsDisplay && (
+            <Notifications/>
+        )}
         <div className="header">
             <div className="left-info">
                 <img className="logo-small" src={logo} alt="logo"></img>
                 <input className="search-box" placeholder="Search Facebook Clone" autoComplete="off" type="search" name="search"></input>
             </div>
             <div className="right-info">
-                <button className="header-button" type="button" name="chat-button"><object type="image/svg+xml" data={chatIcon}></object></button>
-                <button className="header-button" type="button" name="notifcation-button"><object type="image/svg+xml" data={notificationIcon}></object></button>
+                <button className="header-button" type="button" name="chat-button"><img src={chatIcon}></img></button>
+                <button onClick={toggleNotificationsDisplay} className="header-button" type="button" name="notification-button"><img src={notificationIcon}></img></button>
                 <button className="header-button" type="button" name="profile-button"><img src={defaultProfile}></img></button>
             </div>
         </div>
+        </>
     )
 }
 
