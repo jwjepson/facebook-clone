d3a8cb5c53099312322bd3c916989e0e9de26ac4
Jake Jepson <jwjepson1@gmail.com>
2023-03-27 12:47:35 -0400

    import profile content into profile component

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,12 +1,14 @@
 import React from "react";
 import ProfileHeader from "./ProfileHeader";
 import Header from "./Header";
+import ProfileContent from "./ProfileContent";
 
 const Profile = () => {
     return (
         <>
         <Header/>
         <ProfileHeader/>
+        <ProfileContent/>
         </>
     )
 }
