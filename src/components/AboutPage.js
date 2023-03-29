ad02bf8e36a73128b604aff65d8400fe17afd70d
Jake Jepson <jwjepson1@gmail.com>
2023-03-29 17:46:01 -0400

    create AboutPage component for user profile

---
+++ src/components/AboutPage.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Header";
+import ProfileHeader from "./ProfileHeader";
+
+const About = () => {
+    return (
+        <>
+        <Header/>
+        <ProfileHeader/>
+        <div className="profile-content-container">About</div>
+        </>
+    )
+}
+
+export default About;
