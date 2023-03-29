b2121e6484da84316cdf955a4ed8fef936935d11
Jake Jepson <jwjepson1@gmail.com>
2023-03-29 17:46:16 -0400

    create friends page component for user profile

---
+++ src/components/FriendsPage.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Header";
+import ProfileHeader from "./ProfileHeader";
+
+const FriendsPage = () => {
+    return (
+        <>
+        <Header/>
+        <ProfileHeader/>
+        <div className="profile-content-container">Friends</div>
+        </>
+    )
+}
+
+export default FriendsPage
