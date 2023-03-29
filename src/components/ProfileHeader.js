befde652c4bccac8ef6ecf0d4f86f9535b8a9ff4
Jake Jepson <jwjepson1@gmail.com>
2023-03-29 17:54:12 -0400

    use state to keep track of active tab and navigate to correct URL, also get active tab on page refresh

--- src/components/ProfileHeader.js
+++ src/components/ProfileHeader.js
@@ -1,18 +1,53 @@
 import React, { useState } from "react";
 import profilePic from "../images/default-profile-pic.jpg";
 import "../styles/profileheader.css";
+import { Link } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import Header from "./Header";
 
-const ProfileHeader = () => {
+const ProfileHeader = (props) => {
 
-    const [activeTab, setActiveTab] = useState(0);
+    const navigate = useNavigate();
+    const location = useLocation();
 
-    const tabs = ["Posts", "About", "Friends", "Photos", "Videos"];
+    const tabs = ["posts", "about", "friends", "photos", "videos"];
 
     const handleTabClick = (tabIndex) => {
         setActiveTab(tabIndex);
+        switch(tabIndex) {
+            case 0:
+                return navigate("/username");
+            case 1: 
+                return navigate("/username/about");
+            case 2: 
+                return navigate("/username/friends");
+            case 3:
+                return navigate("/username/photos");
+            case 4: 
+                return navigate("/username/videos");
+            default:
+                return navigate("/username");
+        }
     }
 
+    const getActiveTab = (pathname) => {
+        switch(pathname) {
+            case "/username/about":
+                return 1;
+            case "/username/friends":
+                return 2;
+            case "/username/photos":
+                return 3;
+            case "/username/videos":
+                return 4;
+            default:
+                return 0;
+        }
+    }
+
+    const initialActiveTab = getActiveTab(location.pathname);
+    const [activeTab, setActiveTab] = useState(initialActiveTab);
+    
     return (
         <>
         <div className="profile-header-container">
