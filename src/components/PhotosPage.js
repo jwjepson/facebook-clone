cec609a4639da3d66385506dd0dc0da669156f53
Jake Jepson <jwjepson1@gmail.com>
2023-03-29 17:46:29 -0400

    create photos page for user profile

---
+++ src/components/PhotosPage.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Header";
+import ProfileHeader from "./ProfileHeader";
+
+const PhotosPage = () => {
+    return (
+        <>
+        <Header/>
+        <ProfileHeader/>
+        <div className="profile-content-container">Photos Page</div>
+        </>
+    )
+}
+
+export default PhotosPage;
