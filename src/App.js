6a31b85b9d4a953d2352808f8f18bb90e51069f7
Jake Jepson <jwjepson1@gmail.com>
2023-03-29 17:55:26 -0400

    create routes for each of the individual user profile pages

--- src/App.js
+++ src/App.js
@@ -9,7 +9,12 @@ import Home from "./components/Home";
 import CreatePost from "./components/CreatePost";
 import Notifications from "./components/Notifications";
 import ProfileHeader from "./components/ProfileHeader";
-import Profile from "./components/Profile";
+import Intro from "./components/Intro";
+import AboutPage from "./components/AboutPage";
+import PostsPage from "./components/PostsPage";
+import FriendsPage from "./components/FriendsPage";
+import PhotosPage from "./components/PhotosPage";
+import VideosPage from "./components/VideosPage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./styles/app.css";
 
@@ -21,7 +26,11 @@ const App = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={isLoggedIn ? <Home/> : <Login/>}/>
-        <Route path="/username" element={<Profile/>}/>
+        <Route path="/username" element={<PostsPage/>}/>
+        <Route path="/username/about" element={<AboutPage/>}/>
+        <Route path="/username/friends" element={<FriendsPage/>}/>
+        <Route path="/username/photos" element={<PhotosPage/>}/>
+        <Route path="/username/videos" element={<VideosPage/>}/>
       </Routes>
     </BrowserRouter>
     </>
