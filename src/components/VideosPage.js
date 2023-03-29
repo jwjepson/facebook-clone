69bd874ce4f62f57cd1c9322b1f559e8cc822d63
Jake Jepson <jwjepson1@gmail.com>
2023-03-29 17:49:45 -0400

    create videos page for user profile

---
+++ src/components/VideosPage.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Header";
+import ProfileHeader from "./ProfileHeader";
+
+const VideosPage = () => {
+    return (
+        <>
+        <Header/>
+        <ProfileHeader/>
+        <div className="profile-content-container">Videos Page</div>
+        </>
+    )
+}
+
+export default VideosPage;
