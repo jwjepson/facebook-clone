f8e12268a220ed57518ba8a3d2aca3ac04011a6f
Jake Jepson <jwjepson1@gmail.com>
2023-04-01 00:39:53 -0400

    take signOut function as props and pass to onClick attribute for Sign Out item

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -5,16 +5,19 @@ import groupsIcon from "../icons/groups-icon.svg";
 import logoutIcon from "../icons/logout-icon.svg";
 import { Link } from "react-router-dom";
 
-const SideNav = () => {
+const SideNav = ({ signOut }) => {
+
     return (
         <div className="sidenav-container">
             <ul>
                 <Link to="/username">
                     <li className="sidenav-item"><img className="sidenav-icons"src={defaultProfile}></img>Username</li>
                 </Link>
-                <li className="sidenav-item"><img className="sidenav-icons"src={friendsIcon}></img>Friends</li>
+                <Link to="/username/friends">
+                    <li className="sidenav-item"><img className="sidenav-icons"src={friendsIcon}></img>Friends</li>
+                </Link>
                 <li className="sidenav-item"><img className="sidenav-icons"src={groupsIcon}></img>Groups</li>
-                <li className="sidenav-item"><img className="sidenav-icons"src={logoutIcon}></img>Log Out</li>
+                <li onClick={signOut} className="sidenav-item"><img className="sidenav-icons"src={logoutIcon}></img>Log Out</li>
             </ul>
         </div>
     )
