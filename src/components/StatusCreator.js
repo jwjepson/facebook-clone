99b2d329f657c2764885594b3514983a0b88578e
Jake Jepson <jwjepson1@gmail.com>
2023-04-01 14:40:26 -0400

    render the users first name from userData prop

--- src/components/StatusCreator.js
+++ src/components/StatusCreator.js
@@ -3,12 +3,13 @@ import profilePic from "../images/default-profile-pic.jpg";
 import "../styles/statuscreator.css";
 import addPhotoIcon from "../icons/add-photo-icon.svg";
 
-const StatusCreator = (props) => {
+const StatusCreator = ({open, userData}) => {
+
     return (
         <div className="status-creator-container">
             <div className="top-info">
                 <img className="header-button" src={profilePic}></img>
-                <button onClick={props.open} className="status-creator-button" type="button" name="status-button">What's on your mind, User?</button>
+                <button onClick={open} className="status-creator-button" type="button" name="status-button">What's on your mind, {userData.firstName}?</button>
             </div>
             <div className="bottom-options">
                 <ul>
