fa77c15f58a9934602792b3093b83ceca0debc0c
Jake Jepson <jwjepson1@gmail.com>
2023-04-01 00:38:01 -0400

    create handleChange and state for email/passwords form values, and handleLogin to authenicate login with firebase, & pass auth, setUser to Signup component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,31 @@
 import React, {useState} from "react"
 import "../styles/login.css";
 import Signup from "./Signup";
+import { signInWithEmailAndPassword } from "firebase/auth";
 
-const Login = () => {
+const Login = ({ setUser, auth }) => {
     const [signUpDisplay, setsignUpDisplay] = useState(false);
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleChange = (e) => {
+        const { name, value} = e.target;
+        if (name === "email") {
+            setEmail(value);
+        } else if (name === "password") {
+            setPassword(value);
+        }
+    }
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        try {
+            const userCredential = await signInWithEmailAndPassword(auth, email, password)
+            setUser(userCredential.user);
+        } catch (error) {
+            console.log(error);
+        }
+    }
 
     const toggleSignUp = () => {
         setsignUpDisplay(!signUpDisplay);
@@ -11,7 +33,7 @@ const Login = () => {
     return (
         <>
         {signUpDisplay && (
-            <Signup close={toggleSignUp}/>
+            <Signup setUser={setUser} auth={auth} close={toggleSignUp}/>
         )}
         <div className={`login-page-container ${signUpDisplay ? "overlay" : ""}`}>
             <div className="title-info">
@@ -20,9 +42,9 @@ const Login = () => {
             </div>
             <div className="login-form-container">
                 <div>
-                    <form>
-                        <input className="login-input" type="email" name="email" id="email" autocomplete="off" placeholder="Email"></input>
-                        <input className="login-input" type="password" name="password" id="password" placeholder="Password"></input>
+                    <form onSubmit={handleLogin}>
+                        <input className="login-input" onChange={handleChange} value={email} type="email" name="email" id="email" autoComplete="off" placeholder="Email"></input>
+                        <input className="login-input" onChange={handleChange} value={password} type="password" name="password" id="password" placeholder="Password"></input>
                         <button className="login-button" type="submit">Log In</button>
                     </form>
                     <a className="forgot-password">Forgot password?</a>
