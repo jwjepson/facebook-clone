75ce94b7b251905ecd04c48d84d94e57061aa316
Jake Jepson <jwjepson1@gmail.com>
2023-04-01 14:38:12 -0400

    pass userData prop to SideNav component

--- src/components/LeftSideBar.js
+++ src/components/LeftSideBar.js
@@ -3,10 +3,10 @@ import SideNav from "./SideNav";
 import Shortcuts from "./Shortcuts";
 import "../styles/sidebars.css";
 
-const LeftSideBar = (props) => {
+const LeftSideBar = ({signOut, userData}) => {
     return (
         <div className="left-sidebar">
-            <SideNav signOut={props.signOut}/>
+            <SideNav userData={userData} signOut={signOut}/>
             <Shortcuts/>
         </div>
     )
