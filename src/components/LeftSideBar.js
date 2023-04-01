417e313c49d0c4bdbe93544edc2c202219ae9def
Jake Jepson <jwjepson1@gmail.com>
2023-04-01 00:35:24 -0400

    pass signOut function to sidenav component through props

--- src/components/LeftSideBar.js
+++ src/components/LeftSideBar.js
@@ -3,10 +3,10 @@ import SideNav from "./SideNav";
 import Shortcuts from "./Shortcuts";
 import "../styles/sidebars.css";
 
-const LeftSideBar = () => {
+const LeftSideBar = (props) => {
     return (
         <div className="left-sidebar">
-            <SideNav/>
+            <SideNav signOut={props.signOut}/>
             <Shortcuts/>
         </div>
     )
