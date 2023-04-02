20aee75173a810241507f08b8a47c6be71c81d4d
Jake Jepson <jwjepson1@gmail.com>
2023-04-02 13:33:08 -0400

    pass user prop to sidenav component to link user profile url to logged in users UID

--- src/components/LeftSideBar.js
+++ src/components/LeftSideBar.js
@@ -3,10 +3,10 @@ import SideNav from "./SideNav";
 import Shortcuts from "./Shortcuts";
 import "../styles/sidebars.css";
 
-const LeftSideBar = ({signOut, userData}) => {
+const LeftSideBar = ({signOut, userData, user}) => {
     return (
         <div className="left-sidebar">
-            <SideNav userData={userData} signOut={signOut}/>
+            <SideNav user={user} userData={userData} signOut={signOut}/>
             <Shortcuts/>
         </div>
     )
