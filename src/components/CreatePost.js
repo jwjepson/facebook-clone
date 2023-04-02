5bc430d0211a488d79063285fa3c9f5c44ad7e4e
Jake Jepson <jwjepson1@gmail.com>
2023-04-01 22:10:07 -0400

    render users name inside placeholder & destructure props

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -4,18 +4,18 @@ import addPhotoIcon from "../icons/add-photo-icon.svg";
 import closeButton from "../icons/close-button.svg";
 import "../styles/createpost.css";
 
-const CreatePost = (props) => {
+const CreatePost = ({close, userData}) => {
     return (
         <div className="create-post-container">
             <div className="create-post-header">
                 <h4 className="create-post-title">Create post</h4>
-                <img onClick={props.close} className="close-button" src={closeButton}></img>
+                <img onClick={close} className="close-button" src={closeButton}></img>
             </div>
             <div className="create-post-data">
                 <img className="header-button" src={profilePic}></img>
-                <div className="create-post-user">Username</div>
+                <div className="create-post-user">{userData.firstName} {userData.lastName}</div>
             </div>
-            <textarea className="create-post-content" placeholder="What's on your mind, User?"></textarea>
+            <textarea className="create-post-content" placeholder={`What's on your mind, ${userData.firstName}?`}></textarea>
             <div className="create-post-options">
                 <ul>
                     <li><img className="add-photo-icon" src={addPhotoIcon}></img>Photo/video</li>
