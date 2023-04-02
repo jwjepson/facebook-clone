006780452ee5509e29eda9320621c50b279b0dcf
Jake Jepson <jwjepson1@gmail.com>
2023-04-02 13:37:04 -0400

    fetch user profile data from database using useParams with userId

--- src/components/PostsPage.js
+++ src/components/PostsPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PhotosListSmall from "./PhotosListSmall";
 import StatusCreator from "./StatusCreator";
 import FriendsListSmall from "./FriendsListSmall";
@@ -7,12 +7,39 @@ import Intro from "./Intro";
 import ProfileHeader from "./ProfileHeader";
 import Header from "./Header";
 import "../styles/profilecontent.css";
+import { useParams } from "react-router-dom";
+import { getDoc, doc } from "firebase/firestore";
 
-const PostsPage = ({userData}) => {
+const PostsPage = ({user, userData, db}) => {
+
+    const [profileData, setProfileData] = useState(null);
+    const [isLoading, setisLoading] = useState(true);
+
+    let {userId} = useParams();
+
+    useEffect(() => {
+        const getProfileData = async () => {
+            const docRef = doc(db, "users", userId);
+            const docSnap = await getDoc(docRef);
+            if (docSnap.exists()) {
+                setProfileData(docSnap.data());
+                setisLoading(false);
+            } else {
+                console.log("Profile does not exist");
+            }
+        }
+
+        getProfileData();
+    }, [userId, db]);
+
+    if (isLoading) {
+        return null;
+    }
+
     return (
         <>
-            <Header/>
-            <ProfileHeader userData={userData}/>
+            <Header user={user}/>
+            <ProfileHeader userData={profileData}/>
             <div className="profile-content-container">
                 <div className="profile-home-content">
                     <div className="profile-left-sidebar">
@@ -21,9 +48,9 @@ const PostsPage = ({userData}) => {
                         <FriendsListSmall/>
                     </div>
                     <div className="profile-timeline">
-                        <StatusCreator userData={userData}/>
-                        <Status userData={userData}/>
-                        <Status userData={userData}/>
+                        <StatusCreator userData={profileData}/>
+                        <Status userData={profileData}/>
+                        <Status userData={profileData}/>
                     </div>
                 </div>
             </div>
