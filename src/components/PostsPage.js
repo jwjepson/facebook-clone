1c4fdd0c50ee60d726892f3c093382908413ea07
Jake Jepson <jwjepson1@gmail.com>
2023-04-03 16:33:44 -0400

    remove backend call, as it's now being called in parent Profile component

--- src/components/PostsPage.js
+++ src/components/PostsPage.js
@@ -4,42 +4,11 @@ import StatusCreator from "./StatusCreator";
 import FriendsListSmall from "./FriendsListSmall";
 import Status from "./Status";
 import Intro from "./Intro";
-import ProfileHeader from "./ProfileHeader";
-import Header from "./Header";
 import "../styles/profilecontent.css";
-import { useParams } from "react-router-dom";
-import { getDoc, doc } from "firebase/firestore";
 
 const PostsPage = ({user, userData, db}) => {
-
-    const [profileData, setProfileData] = useState(null);
-    const [isLoading, setisLoading] = useState(true);
-
-    let {userId} = useParams();
-
-    useEffect(() => {
-        const getProfileData = async () => {
-            const docRef = doc(db, "users", userId);
-            const docSnap = await getDoc(docRef);
-            if (docSnap.exists()) {
-                setProfileData(docSnap.data());
-                setisLoading(false);
-            } else {
-                console.log("Profile does not exist");
-            }
-        }
-
-        getProfileData();
-    }, [userId, db]);
-
-    if (isLoading) {
-        return null;
-    }
-
     return (
         <>
-            <Header user={user}/>
-            <ProfileHeader userData={profileData}/>
             <div className="profile-content-container">
                 <div className="profile-home-content">
                     <div className="profile-left-sidebar">
@@ -48,9 +17,9 @@ const PostsPage = ({user, userData, db}) => {
                         <FriendsListSmall/>
                     </div>
                     <div className="profile-timeline">
-                        <StatusCreator userData={profileData}/>
-                        <Status userData={profileData}/>
-                        <Status userData={profileData}/>
+                        <StatusCreator userData={userData}/>
+                        <Status userData={userData}/>
+                        <Status userData={userData}/>
                     </div>
                 </div>
             </div>
