ed13cc40a465b30e87a3cf64c38b1be4a6256752
Jake Jepson <jwjepson1@gmail.com>
2023-04-03 16:14:42 -0400

    create a Profile component to pass profileData to all the individual profile page routes, to prevent multiple backend calls for each page

---
+++ src/components/Profile.js
@@ -0,0 +1,51 @@
+import React, {useState, useEffect} from "react";
+import PostsPage from "./PostsPage";
+import { useParams, Routes, Route } from "react-router-dom";
+import { BeatLoader } from "react-spinners";
+import AboutPage from "./AboutPage";
+import PhotosPage from "./PhotosPage";
+import FriendsPage from "./FriendsPage";
+import VideosPage from "./VideosPage";
+import {doc, getDoc} from "firebase/firestore";
+
+const Profile = ({user, db, userData}) => {
+
+    const [profileData, setProfileData] = useState(null);
+    const [isLoading, setisLoading] = useState(true);
+
+    let {userId} = useParams();
+
+    useEffect(() => {
+        const getProfileData = async () => {
+            const docRef = doc(db, "users", userId);
+            const docSnap = await getDoc(docRef);
+            if (docSnap.exists()) {
+                setProfileData(docSnap.data());
+                setisLoading(false);
+            } else {
+                console.log("Profile does not exist");
+            }
+        }
+
+        getProfileData();
+    }, [userId, db]);
+
+    if (isLoading) {
+        return null;
+    }
+
+    console.log(userId);
+    return (
+        <>
+        <Routes>
+            <Route path="/" element={<PostsPage user={user} db={db} userData={profileData} />}/>
+            <Route path="/about" element={userData ? <AboutPage db={db} user={user} userData={profileData}/> : <BeatLoader/>}/>
+            <Route path="/friends" element={userData ? <FriendsPage user={user} userData={profileData}/> : <BeatLoader/>}/>
+            <Route path="/photos" element={userData ? <PhotosPage user={user} userData={profileData}/> : <BeatLoader/>}/>
+            <Route path="/videos" element={userData ? <VideosPage user={user} userData={profileData}/> : <BeatLoader/>}/>
+        </Routes>
+        </>
+    )
+}
+
+export default Profile;
