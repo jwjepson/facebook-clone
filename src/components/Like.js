289d1ff74177fc28b85cc7708edc39ad3791a4b1
Jake Jepson <jwjepson1@gmail.com>
2023-04-04 12:25:46 -0400

    implement like function on posts, adding user to likes array if not already, and removing if already

--- src/components/Like.js
+++ src/components/Like.js
@@ -1,10 +1,35 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import likeIcon from "../icons/like-icon.svg";
+import { arrayRemove, arrayUnion, doc, getDoc, updateDoc } from "firebase/firestore";
 
-const Like = () => {
+const Like = ({postData, db, user}) => {
+
+    const handleLike = async (e) => {
+        let postId = e.currentTarget.getAttribute("data-postid");
+        const docRef = doc(db, "posts", postId);
+        try {
+            const docSnap = await getDoc(docRef);
+            if (docSnap.exists()) {
+                if ((docSnap.data().likes).includes(user.uid)) {
+                    await updateDoc(docRef, {
+                        likes: arrayRemove(user.uid)
+                    });
+                } else {
+                    await updateDoc(docRef, {
+                        likes: arrayUnion(user.uid)
+                    })
+                }
+            } else {
+                console.log("no");
+            }
+        } catch (error) {
+            console.log("Could not like Post ", error);
+        }
+    }
+
     return (
         <div className="like-button">
-            <button type="button" name="like-button"><img className="like-icon" src={likeIcon}></img>Like</button>
+            <button onClick={handleLike} type="button" data-postid={postData.id} name="like-button"><img className="like-icon" src={likeIcon}></img>Like</button>
         </div>
     )
 }
