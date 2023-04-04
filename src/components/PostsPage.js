a4c8530fc68155849876e7462a0e9cd01b83884a
Jake Jepson <jwjepson1@gmail.com>
2023-04-03 22:11:19 -0400

    get posts from database based on user profile and render that users posts to their profile

--- src/components/PostsPage.js
+++ src/components/PostsPage.js
@@ -5,8 +5,29 @@ import FriendsListSmall from "./FriendsListSmall";
 import Status from "./Status";
 import Intro from "./Intro";
 import "../styles/profilecontent.css";
+import { collection, query, where, getDocs, orderBy } from "firebase/firestore";
+import { useParams } from "react-router-dom";
+
 
 const PostsPage = ({user, userData, db}) => {
+
+    let {userId} = useParams();
+
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        const getUserPosts = async () => {
+            const q = query(collection(db, "posts"), where("postedBy", "==", userId), orderBy("timestamp", "desc"));
+            const querySnapshot = await getDocs(q);
+            const docsData = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+            setPosts(docsData);
+        }
+
+        getUserPosts();
+    }, [userId, db]);
+
+    console.log(posts);
+
     return (
         <>
             <div className="profile-content-container">
@@ -18,8 +39,9 @@ const PostsPage = ({user, userData, db}) => {
                     </div>
                     <div className="profile-timeline">
                         <StatusCreator userData={userData}/>
-                        <Status userData={userData}/>
-                        <Status userData={userData}/>
+                        {posts.map((post) => (
+                            <Status key={post.id} userData={userData} postData={post}/>
+                        ))}
                     </div>
                 </div>
             </div>
