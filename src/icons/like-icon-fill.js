e12cdbf68e5b08c0b9bd580e8c843b79bb28d103
Jake Jepson <jwjepson1@gmail.com>
2023-04-04 16:13:53 -0400

    turn like icons into components for conditionally rendering

---
+++ src/icons/like-icon-fill.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const LikeIconFilled = () => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" fill="#0C87EE" height="25" viewBox="0 96 960 960" width="25"><path d="M729 960H241V418l290-301 43 37q10.875 9.129 14.938 20.065Q593 185 593 199.32V211l-46 207h316q28.2 0 50.6 22.4Q936 462.8 936 491v85.33q0 11.291-3 26.524T925 629L805.33 906.745q-10.675 22.273-31.71 37.764Q752.586 960 729 960ZM181 418v542H63V418h118Z"/></svg>
+    );
+};
+
+export default LikeIconFilled;
