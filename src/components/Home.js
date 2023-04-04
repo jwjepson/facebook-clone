2e22e2e06377297b5fc7ca7ffffbc6212df9189b
Jake Jepson <jwjepson1@gmail.com>
2023-04-04 12:29:06 -0400

    get TimelinePosts, store inside state and render each status mapping through state

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import SideNav from "./SideNav";
 import StatusCreator from "./StatusCreator";
 import Status from "./Status";
@@ -7,25 +7,42 @@ import RightSideBar from "./RightSideBar";
 import CreatePost from "./CreatePost";
 import "../styles/home.css";
 import Header from "./Header";
+import { collection, query, where, onSnapshot, orderBy } from "firebase/firestore";
 
-const Home = ({signOut, userData, user}) => {
+const Home = ({signOut, userData, user, db}) => {
     const [createPostDisplay, setcreatePostDisplay] = useState(false);
+    const [posts, setPosts] = useState([]);
 
     const toggleCreatePost = () => {
         setcreatePostDisplay(!createPostDisplay);
     }
 
+    useEffect(() => {
+        const getTimelinePosts = async () => {
+            const q = query(collection(db, "posts"), where("postedBy", "==", user.uid), orderBy("timestamp", "desc"));
+            const unsubscribe = onSnapshot(q, (querySnapshot) => {
+                const docsData = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+                setPosts(docsData);
+            });
+            return unsubscribe;
+        };
+
+        getTimelinePosts();
+    }, [user, db]);
+
     return (
         <>
         <Header user={user}/>
         {createPostDisplay && (
-            <CreatePost userData={userData} close={toggleCreatePost}/>
+            <CreatePost user={user} db={db} userData={userData} close={toggleCreatePost}/>
         )}
         <div className={`home-page ${createPostDisplay ? "overlay" : ""}`}>
             <LeftSideBar user={user} userData={userData} signOut={signOut}/>
             <div className="timeline">
                 <StatusCreator userData={userData} open={toggleCreatePost}/>
-                <Status userData={userData}/>
+                {posts.map((post) => (
+                            <Status key={post.id} user={user} db={db} userData={userData} postData={post}/>
+                ))}
             </div>
             <RightSideBar/>
         </div>
