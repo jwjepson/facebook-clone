ed6cc31e4f9a77458b6fa353e6df196e9a381f0d
Jake Jepson <jwjepson1@gmail.com>
2023-04-05 22:38:30 -0400

    get data for users friends, store inside state and render inside small friends list limiting to 9

--- src/components/FriendsListSmall.js
+++ src/components/FriendsListSmall.js
@@ -1,53 +1,45 @@
-import React from "react";
+import React, { useEffect, useState} from "react";
 import "../styles/friendslistsmall.css";
 import profilePic from "../images/default-profile-pic.jpg";
+import { collection, query, where, getDocs, limit } from "firebase/firestore";
 
-const FriendsListSmall = () => {
+const FriendsListSmall = ({userData, db}) => {
+
+    const [friendsData, setFriendsData] = useState([]);
+
+    useEffect(() => {
+        const getFriendsData = async () => {
+            const q = query(collection(db, "users"), where("id", "in", userData.friends), limit(9));
+            const querySnapshot = await getDocs(q);
+            const friendsData = querySnapshot.docs.map((doc) => doc.data());
+            setFriendsData(friendsData);
+        }
+        getFriendsData();
+    }, [userData.friends]);
+
     return (
         <div className="friends-list-container small">
             <div className="friends-list-container-header">
                 <h4>Friends</h4>
                 <a className="see-all-button friends">See all friends</a>
             </div>
-            <span>500 friends</span>
+            <span>
+                {userData.friends.length === 1 ? (
+                    `${userData.friends.length} friend`
+                ) : (
+                    `${userData.friends.length} friends`
+                )}
+            </span>
             <div className="friends-list-container-content">
                 <div className="friends-list-grid">
-                    <div className="friend">
-                        <div className="thumbnail friend">
-                            <img className="photo" src={profilePic}></img>
+                    {friendsData.map((friend) => (
+                        <div className="friend">
+                            <div className="thumbnail friend">
+                                <img className="photo" src={profilePic}></img>
+                            </div>
+                            <h4 className="friend-name">{friend.firstName} {friend.lastName}</h4>
                         </div>
-                        <h4 className="friend-name">User Name</h4>
-                    </div>
-                    <div className="friend">
-                        <div className="thumbnail friend">
-                            <img className="photo" src={profilePic}></img>
-                        </div>
-                        <h4 className="friend-name">User Name</h4>
-                    </div>
-                    <div className="friend">
-                        <div className="thumbnail friend">
-                            <img className="photo" src={profilePic}></img>
-                        </div>
-                        <h4 className="friend-name">User Name</h4>
-                    </div>
-                    <div className="friend">
-                        <div className="thumbnail friend">
-                            <img className="photo" src={profilePic}></img>
-                        </div>
-                        <h4 className="friend-name">User Name</h4>
-                    </div>
-                    <div className="friend">
-                        <div className="thumbnail friend">
-                            <img className="photo" src={profilePic}></img>
-                        </div>
-                        <h4 className="friend-name">User Name</h4>
-                    </div>
-                    <div className="friend">
-                        <div className="thumbnail friend">
-                            <img className="photo" src={profilePic}></img>
-                        </div>
-                        <h4 className="friend-name">User Name</h4>
-                    </div>
+                    ))}
                 </div>
             </div>
         </div>
