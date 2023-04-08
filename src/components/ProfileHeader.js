74cbb7771ab7abd0ede9e14012a252f957dbe6c5
Jake Jepson <jwjepson1@gmail.com>
2023-04-08 15:28:12 -0400

    wrap profile header in a div for alignment, create respond profile action button, and conditionally rendered div that shows friend request when visiting requested users profile

--- src/components/ProfileHeader.js
+++ src/components/ProfileHeader.js
@@ -4,7 +4,7 @@ import "../styles/profileheader.css";
 import { useParams } from "react-router-dom";
 import { useNavigate, useLocation } from "react-router-dom";
 
-const ProfileHeader = ({userData , user, sendFriendRequest}) => {
+const ProfileHeader = ({userData , user, sendFriendRequest, currentUserData, confirmRequest}) => {
 
     const navigate = useNavigate();
     const location = useLocation();
@@ -56,49 +56,63 @@ const ProfileHeader = ({userData , user, sendFriendRequest}) => {
         const initialActiveTab = getActiveTab(location.pathname);
         setActiveTab(initialActiveTab);
     }, [location.pathname]);
-    
+   
     return (
         <>
         <div className="profile-header-container">
             <div className="profile-cover"></div>
-            <div className="profile-header-middle-section">
-                <div className="profile-header-left-data">
-                    <img className="profile-header-picture" src={profilePic}></img>
-                    <div className="profile-data">
-                        <h1 className="profile-username">{userData.firstName} {userData.lastName}</h1>
-                        <a className="profile-friend-count">
-                            {userData.friends.length === 1 ? (
-                                `${userData.friends.length} friend`
-                            ) : (
-                                `${userData.friends.length} friends`
+                <div className="profile-header-data">
+                    <div className="profile-header-middle-section">
+                        <div className="profile-header-left-data">
+                            <img className="profile-header-picture" src={profilePic}></img>
+                            <div className="profile-data">
+                                <h1 className="profile-username">{userData.firstName} {userData.lastName}</h1>
+                                <a className="profile-friend-count">
+                                    {userData.friends.length === 1 ? (
+                                        `${userData.friends.length} friend`
+                                    ) : (
+                                        `${userData.friends.length} friends`
+                                    )}
+                                </a>
+                            </div>
+                        </div>
+                        <div className="profile-actions">
+                            {currentUserData.friends.includes(userData.id) && (
+                                <button type="button">Friends</button>
                             )}
-                        </a>
+                            {!currentUserData.friends.includes(userData.id) && userData.id !== user.uid && !currentUserData.friendRequests.includes(userData.id) && (
+                            <button onClick={sendFriendRequest} data-user={userData.id} type="button">
+                                {isFriendRequestSent ? "Requested" : "Add Friend"}
+                                </button>
+                            )}
+                            {currentUserData.friendRequests.includes(userData.id) && (
+                                <button onClick={sendFriendRequest} data-user={userData.id} type="button">Respond</button>
+                            )}
+                            {userData.id === user.uid && (
+                                <button type="button">Edit Profile</button>
+                            )}
+                            {userData.id !== user.uid && (
+                                <button type="button">Message</button>
+                            )}
+                        </div>
+                    </div>
+                    {currentUserData.friendRequests.includes(userData.id) && !userData.friends.includes(user.uid) && (
+                        <div className="friend-request-container profile">
+                            <h4>{userData.firstName} sent you a friend request</h4>
+                            <div className="friend-request-container-options">
+                                <button onClick={confirmRequest} data-id={userData.id} className="confirm-button" type="button">Confirm request</button>
+                                <button className="delete-button" type="button">Delete request</button>
+                            </div>
+                        </div>
+                    )}
+                    <div className="profile-navigation">
+                        <ul>
+                            {tabs.map((tab, index) => (
+                                <li key={index} onClick={() => handleTabClick(index)} className={`profile-nav ${activeTab === index ? "active" : ""}`}>{tab}</li>
+                            ))}
+                        </ul>
                     </div>
                 </div>
-                <div className="profile-actions">
-                    {userData.friends.includes(user.uid) && (
-                        <button type="button">Friends</button>
-                    )}
-                    {!userData.friends.includes(user.uid) && userData.id !== user.uid && (
-                       <button onClick={sendFriendRequest} data-user={userData.id} type="button">
-                        {isFriendRequestSent ? "Requested" : "Add Friend"}
-                        </button>
-                    )}
-                    {userData.id === user.uid && (
-                        <button type="button">Edit Profile</button>
-                    )}
-                    {userData.id !== user.uid && (
-                        <button type="button">Message</button>
-                    )}
-                </div>
-            </div>
-            <div className="profile-navigation">
-                <ul>
-                    {tabs.map((tab, index) => (
-                        <li key={index} onClick={() => handleTabClick(index)} className={`profile-nav ${activeTab === index ? "active" : ""}`}>{tab}</li>
-                    ))}
-                </ul>
-            </div>
         </div>
         </>
     )
