40bb95f12748d32add387b694c95c60fce7321df
Jake Jepson <jwjepson1@gmail.com>
2023-04-10 20:11:56 -0400

    add comment to database under comments collection with identifying properties for rendering purposes

--- src/components/WriteComment.js
+++ src/components/WriteComment.js
@@ -1,12 +1,39 @@
-import React from "react";
+import React, {useState} from "react";
 import "../styles/writecomment.css";
 import profilePic from "../images/default-profile-pic.jpg";
+import { collection, addDoc } from "firebase/firestore";
 
-const WriteComment = () => {
+const WriteComment = ({db, postData, user, userData, currentUserData}) => {
+    const [comment, setComment] = useState("");
+
+    const handleChange = (e) => {
+        setComment(e.target.value);
+    }
+
+    const addComment = async (e) => {
+        e.preventDefault();
+        const postId = e.target.getAttribute("data-postid");
+        await addDoc(collection(db, "comments"), {
+            postId: postId,
+            content: comment,
+            timestamp: new Date(),
+            postedBy: {
+                userId: user.uid,
+                firstName: currentUserData.firstName,
+                lastName: currentUserData.lastName,
+            }
+        });
+        setComment("");
+    }
+
+    console.log(currentUserData);
+
     return (
         <div className="write-comment-container">
             <img src={profilePic} className="write-comment-avatar"></img>
-            <input className="write-comment-input" name="write-comment" placeholder="Write a comment..." type="text"></input>
+            <form onSubmit={addComment} data-postid={postData.id}>
+                <input className="write-comment-input" autoComplete="off" onChange={handleChange} value={comment} name="write-comment" placeholder="Write a comment..." type="text"></input>
+            </form>
         </div>
     )
 }
