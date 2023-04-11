c6fb4b98b76dab5be62a8ff3d04ac2409d4d40da
Jake Jepson <jwjepson1@gmail.com>
2023-04-11 02:03:56 -0400

    format timestamp & render, conditionally render delete comment button on hover if user owns comment

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -2,21 +2,32 @@ import React, { useEffect, useState } from "react";
 import "../styles/comment.css";
 import profilePic from "../images/default-profile-pic.jpg";
 import { doc, getDoc } from "firebase/firestore";
+import closeIcon from "../icons/close-button.svg";
 import { Link } from "react-router-dom";
 
-const Comment = ({commentData}) => {
+const Comment = ({commentData, user, formatDate}) => {
+
+    const [showDeleteButton, setShowDeleteButton] = useState(false);
+
+    const formattedDate = formatDate(commentData.timestamp);
 
     return (
-        <div className="comment-container">
-            <Link to={`/${commentData.postedBy.userId}`}>
-                <img src={profilePic} className="write-comment-avatar"></img>
-            </Link>
-            <div className="comment-data">
+        <div className="comment-container" onMouseEnter={() => setShowDeleteButton(true)} onMouseLeave={() => setShowDeleteButton(false)}>
+            <div>
                 <Link to={`/${commentData.postedBy.userId}`}>
-                    <div className="comment-user">{commentData.postedBy.firstName} {commentData.postedBy.lastName}</div>
+                    <img src={profilePic} className="write-comment-avatar"></img>
                 </Link>
-                <p className="comment">{commentData.content}</p>
+                <div className="comment-data">
+                    <Link to={`/${commentData.postedBy.userId}`}>
+                        <div className="comment-user">{commentData.postedBy.firstName} {commentData.postedBy.lastName}</div>
+                    </Link>
+                    <p className="comment">{commentData.content}</p>
+                </div>
+                {user.uid === commentData.postedBy.userId && (
+                <button className={`delete-comment-button ${showDeleteButton ? "active" : ""}`} type="button"><img className="delete-comment-icon" src={closeIcon}></img></button>
+            )}
             </div>
+                <div className="comment-timestamp">{formattedDate}</div>
         </div>
     )
 }
