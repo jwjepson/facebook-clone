82c11d095a73a49439393afef1fa47f1e87c9be1
Jake Jepson <jwjepson1@gmail.com>
2023-04-11 17:48:43 -0400

    create Delete component that deletes content based on it's type and ID

---
+++ src/components/Delete.js
@@ -0,0 +1,31 @@
+import React from "react";
+import deleteIcon from "../icons/close-button.svg";
+import { doc, where, getDocs, query, collection, writeBatch } from "firebase/firestore";
+
+const Delete = ({type, id, db}) => {
+
+    const handleDelete = async () => {
+        const batch = writeBatch(db);
+
+        if (type === "posts") {
+            const commentQuery = query(collection(db, "comments"), where("postId", "==", id));
+            const commentSnapshot = await getDocs(commentQuery);
+
+            commentSnapshot.forEach((document) => {
+                batch.delete(doc(db, "comments", document.id));
+            });
+        }
+        
+        batch.delete(doc(db, type, id));
+
+        await batch.commit();
+    }
+
+    return (
+        <>
+            <button onClick={handleDelete} className="delete-comment-button" type="button" name="close-button"><img className="delete-comment-icon" src={deleteIcon}></img></button>
+        </>
+    )
+}
+
+export default Delete;
