9025a7ad9a732966600c29493d3f1b3c35e7b8f6
Jake Jepson <jwjepson1@gmail.com>
2023-04-10 20:39:13 -0400

    add links to user profile

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -2,14 +2,19 @@ import React, { useEffect, useState } from "react";
 import "../styles/comment.css";
 import profilePic from "../images/default-profile-pic.jpg";
 import { doc, getDoc } from "firebase/firestore";
+import { Link } from "react-router-dom";
 
 const Comment = ({commentData}) => {
 
     return (
         <div className="comment-container">
-            <img src={profilePic} className="write-comment-avatar"></img>
+            <Link to={`/${commentData.postedBy.userId}`}>
+                <img src={profilePic} className="write-comment-avatar"></img>
+            </Link>
             <div className="comment-data">
-                <div className="comment-user">{commentData.postedBy.firstName} {commentData.postedBy.lastName}</div>
+                <Link to={`/${commentData.postedBy.userId}`}>
+                    <div className="comment-user">{commentData.postedBy.firstName} {commentData.postedBy.lastName}</div>
+                </Link>
                 <p className="comment">{commentData.content}</p>
             </div>
         </div>
