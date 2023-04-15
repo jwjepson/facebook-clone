ceb9b728bd12acae51ad7ae3930c3873fa3e8de0
Jake Jepson <jwjepson1@gmail.com>
2023-04-15 13:05:59 -0400

    allow users to upload photos in their status and store that URL in post collection

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,13 +1,18 @@
-import React, {useState} from "react";
+import React, {useState, useRef, useEffect} from "react";
 import profilePic from "../images/default-profile-pic.jpg";
 import addPhotoIcon from "../icons/add-photo-icon.svg";
 import closeButton from "../icons/close-button.svg";
 import "../styles/createpost.css";
 import {addDoc, collection} from "firebase/firestore";
+import {ref, getDownloadURL, uploadBytes} from "firebase/storage";
+import {v4} from "uuid";
 
-const CreatePost = ({close, userData, db, user}) => {
+const CreatePost = ({close, userData, db, user, storage}) => {
 
     const [postContent, setPostContent] = useState("");
+    const [mediaFile, setMediaFile] = useState(null);
+    const [mediaURL, setMediaURL] = useState(null);
+    const mediaFileRef = useRef(null);
 
     const handleChange = (e) => {
         setPostContent(e.target.value);
@@ -21,12 +26,37 @@ const CreatePost = ({close, userData, db, user}) => {
             postedBy: user.uid,
             timestamp: new Date(),
             likes: [],
+            media: mediaURL,
         });
     }
 
+    const handleMediaSelection = (e) => {
+        setMediaFile(e.target.files[0]);
+    }
+
+    const handleMediaClick = (e) => {
+        mediaFileRef.current.click();
+    }
+
+    const uploadMedia = async (e) => {
+        if (mediaFile === null) {
+            return;
+        }
+        const mediaRef = ref(storage, `users/${userData.id}/mediaPosts/${mediaFile.name + v4()}`);
+        uploadBytes(mediaRef, mediaFile).then((snapshot) => {
+            getDownloadURL(snapshot.ref).then((url) => {
+                setMediaURL(url);
+            })
+        })
+    }
+
+    useEffect(() => {
+        uploadMedia();
+    }, [mediaFile]);
+
     return (
         <>
-            <div className="create-post-container">
+            <div className={`create-post-container ${mediaURL ? "has-media" : ""}`}>
                 <div className="create-post-header">
                     <h4 className="create-post-title">Create post</h4>
                     <img onClick={close} className="close-button" src={closeButton}></img>
@@ -35,15 +65,21 @@ const CreatePost = ({close, userData, db, user}) => {
                     <img className="header-button" src={userData.profilePicURL}></img>
                     <div className="create-post-user">{userData.firstName} {userData.lastName}</div>
                 </div>
-                <form onSubmit={handlePost}>
+                <form id="create-post-form" onSubmit={handlePost}>
                     <textarea className="create-post-content" onChange={handleChange} value={postContent} placeholder={`What's on your mind, ${userData.firstName}?`}></textarea>
-                    <div className="create-post-options">
-                        <ul>
-                            <li><img className="add-photo-icon" src={addPhotoIcon}></img>Photo/video</li>
-                        </ul>
-                    </div>
-                    <button type="submit" className="post-button">Post</button>
+                    {mediaURL && (
+                        <div className="media">
+                            <img alt="User uploaded media" src={mediaURL}></img>
+                        </div>
+                    )}
+                    <input hidden type="file" ref={mediaFileRef} onChange={handleMediaSelection}></input>
                 </form>
+                <div className="create-post-options">
+                        <ul>
+                            <li onClick={handleMediaClick}><img className="add-photo-icon" src={addPhotoIcon}></img>Photo/video</li>
+                        </ul>
+                </div>
+                <button type="submit" form="create-post-form" className="post-button">Post</button>
             </div>
             <div className="overlay"></div>
         </>
