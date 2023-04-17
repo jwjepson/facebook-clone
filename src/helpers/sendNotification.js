deda366d990af74ce98de8e29079a3968c84e113
Jake Jepson <jwjepson1@gmail.com>
2023-04-17 17:53:38 -0400

    conditionally set content message based on notification type

--- src/helpers/sendNotification.js
+++ src/helpers/sendNotification.js
@@ -2,18 +2,22 @@ import {addDoc, collection, updateDoc, increment, doc} from "firebase/firestore"
 
 export const sendNotification = async (type, sender, receiver, db) => {
 
-    console.log("Notification is being called");
-    
+    let content;
+
     if (type === "friendRequest") {
-        await addDoc(collection(db, "notifications"), {
-                content: `${sender.firstName} sent you a friend request!`,
-                picture: sender.profilePicURL,
-                read: false,
-                src: sender.id,
-                belongsTo: receiver,
-                timestamp: new Date(),
-        });
+        content = `${sender.firstName} sent you a friend request!`;
+    } else if (type === "confirmRequest") {
+        content = `${sender.firstName} accepted your friend request`;
     }
+   
+    await addDoc(collection(db, "notifications"), {
+        content: content,
+        picture: sender.profilePicURL,
+        read: false,
+        src: sender.id,
+        belongsTo: receiver,
+        timestamp: new Date(),
+    });
 
     const userRef = doc(db, "users", receiver);
     await updateDoc(userRef, {
