70da7d327ac4d5581ca82a8726b24e3c193c7ffa
Jake Jepson <jwjepson1@gmail.com>
2023-04-17 16:56:10 -0400

    conditionally render notifications badge if over 0, and remove notification count when clicked

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import "../styles/header.css";
 import logo from "../icons/fb-clone-logo.png";
 import notificationIcon from "../icons/notification-icon.svg";
@@ -7,13 +7,26 @@ import Notifications from "./Notifications";
 import defaultProfile from "../images/default-profile-pic.jpg";
 import Search from "./Search";
 import { Link } from "react-router-dom";
+import { getDoc, updateDoc, doc } from "firebase/firestore";
 
-const Header = ({user, db, userData}) => {
+const Header = ({user, db, userData, updateUserData}) => {
     const [notificationsDisplay, setnotificationsDisplay] = useState(false);
     const [searchBarDisplay, setSearchBarDisplay] = useState(false);
 
-    const toggleNotificationsDisplay = () => {
+    const toggleNotificationsDisplay = async () => {
         setnotificationsDisplay(!notificationsDisplay);
+        if (userData.notificationCount > 0) {
+            const docRef = doc(db, "users", user.uid);
+            const docSnap = await getDoc(docRef);
+            const updatedUserData = {
+                ...userData,
+                notificationCount: 0,
+            }
+            updateUserData(updatedUserData);
+            await updateDoc(docRef, {
+                notificationCount: 0,
+            })
+        }
     }
 
     const toggleSearchBarDisplay = () => {
@@ -22,8 +35,8 @@ const Header = ({user, db, userData}) => {
 
     return (
         <>
-        {notificationsDisplay && (
-            <Notifications/>
+        {userData && notificationsDisplay && (
+            <Notifications toggleNotificationsDisplay={toggleNotificationsDisplay} user={user} db={db} userData={userData}/>
         )}
         {searchBarDisplay && (
             <Search db={db} toggleSearchBarDisplay={toggleSearchBarDisplay}/>
@@ -37,7 +50,7 @@ const Header = ({user, db, userData}) => {
             </div>
             <div className="right-info">
                 <button className="header-button" type="button" name="chat-button"><img src={chatIcon}></img></button>
-                <button onClick={toggleNotificationsDisplay} className="header-button" type="button" name="notification-button"><img src={notificationIcon}></img></button>
+                <button onClick={toggleNotificationsDisplay} className="header-button notify" type="button" name="notification-button"><img src={notificationIcon}></img>{userData.notificationCount > 0 && (<div className="badge">{userData.notificationCount}</div>)}</button>
                 <Link to={`/${user.uid}`}>
                     <button className="header-button profile" type="button" name="profile-button"><img src={userData.profilePicURL}></img></button>
                 </Link>
