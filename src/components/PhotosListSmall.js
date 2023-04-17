05f02c805150fc15f12c27931fdf80bf95744ffa
Jake Jepson <jwjepson1@gmail.com>
2023-04-16 21:01:14 -0400

    rename component & get users photos, rendering the most recent 9 on profile page

--- src/components/PhotosListSmall.js
+++ src/components/PhotosListSmall.js
@@ -1,9 +1,35 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import "../styles/photoslistsmall.css";
 import profilePic from "../images/default-profile-pic.jpg";
 import { checkPermission } from "../helpers/checkPermission";
+import { useParams } from "react-router-dom";
+import { collection, query, where, orderBy, limit, getDocs } from "firebase/firestore";
 
-const Photos = ({user, userData}) => {
+const PhotosListSmall = ({user, userData, db}) => {
+
+    const [photos, setPhotos] = useState([]);
+
+    let {userId} = useParams();
+
+    useEffect(() => {
+        const getUserMedia = async () => {
+            const docs = [];
+            try {
+                const q = query(collection(db, "media"), where("belongsTo", "==", userId), orderBy("timestamp", "desc"), limit(9));
+                const querySnapshot = await getDocs(q);
+                querySnapshot.forEach((doc) => {
+                    docs.push(doc.data());
+                })
+                setPhotos(docs);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        getUserMedia();
+    }, [userId, db]);
+
+    console.log(photos);
+
     return (
         <div className="photos-container">
             <div className="photos-container-header">
@@ -14,26 +40,11 @@ const Photos = ({user, userData}) => {
                 <div className="photos-grid">
                     {checkPermission(user, userData) && (
                         <>
+                        {photos.map((photo) => (
                             <div className="thumbnail">
-                                <img className="photo" src={profilePic}></img>
+                                <img className="photo" src={photo.mediaURL}></img>
                             </div>
-                            <div className="thumbnail">
-                                <img className="photo" src={profilePic}></img>
-                            </div><div className="thumbnail">
-                                <img className="photo" src={profilePic}></img>
-                            </div><div className="thumbnail">
-                                <img className="photo" src={profilePic}></img>
-                            </div><div className="thumbnail">
-                                <img className="photo" src={profilePic}></img>
-                            </div><div className="thumbnail">
-                                <img className="photo" src={profilePic}></img>
-                            </div><div className="thumbnail">
-                                <img className="photo" src={profilePic}></img>
-                            </div><div className="thumbnail">
-                                <img className="photo" src={profilePic}></img>
-                            </div><div className="thumbnail">
-                                <img className="photo" src={profilePic}></img>
-                            </div>
+                        ))}
                         </>
                     )}
                 </div>
@@ -42,4 +53,4 @@ const Photos = ({user, userData}) => {
     )
 }
 
-export default Photos;
+export default PhotosListSmall;
