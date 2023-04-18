1aa62c3711b78805c5115f8d01bdc3e38fe3ed7d
Jake Jepson <jwjepson1@gmail.com>
2023-04-18 17:20:52 -0400

    create notification class for sending notifications and updating notification count

--- src/helpers/sendNotification.js
+++ src/helpers/sendNotification.js
@@ -1,26 +1,57 @@
 import {addDoc, collection, updateDoc, increment, doc} from "firebase/firestore";
 
-export const sendNotification = async (type, sender, receiver, db) => {
 
-    let content;
-
-    if (type === "friendRequest") {
-        content = `${sender.firstName} sent you a friend request!`;
-    } else if (type === "confirmRequest") {
-        content = `${sender.firstName} accepted your friend request`;
+const notificationContentMap = {
+    friendRequest: (sender) => `${sender.firstName} ${sender.lastName} sent you a friend request`,
+    confirmRequest: (sender) => `${sender.firstName} ${sender.lastName} accepted your friend request`,
+    like: (sender, data) => {
+        if (data.content === "") {
+            return `${sender.firstName} ${sender.lastName} liked your photo`;
+        } else {
+            return `${sender.firstName} ${sender.lastName} liked your post: "${data.content.slice(0,45)}..."`;
+        }
     }
-   
-    await addDoc(collection(db, "notifications"), {
-        content: content,
-        picture: sender.profilePicURL,
-        read: false,
-        src: sender.id,
-        belongsTo: receiver,
-        timestamp: new Date(),
-    });
-
-    const userRef = doc(db, "users", receiver);
-    await updateDoc(userRef, {
-        notificationCount: increment(1),
-    });
 }
+
+const notificationSrcMap = {
+    friendRequest: (sender) => sender.id,
+    confirmRequest: (sender) => sender.id,
+    like: (sender) => sender.id,
+};
+
+
+class Notification {
+    constructor(type, sender, receiver, db, data) {
+        this.type = type;
+        this.sender = sender;
+        this.receiver = receiver;
+        this.db = db;
+        this.data = data;
+    }
+
+    get content() {
+        return notificationContentMap[this.type](this.sender, this.data);
+    }
+
+    get src() {
+        return notificationSrcMap[this.type](this.sender);
+    }
+
+    async send() {
+        await addDoc(collection(this.db, "notifications"), {
+            content: this.content,
+            picture: this.sender.profilePicURL,
+            read: false,
+            src: this.src,
+            belongsTo: this.receiver,
+            timestamp: new Date(),
+        });
+
+        const userRef = doc(this.db, "users", this.receiver);
+        await updateDoc(userRef, {
+            notificationCount: increment(1),
+        });
+    }
+}
+
+export {Notification};
